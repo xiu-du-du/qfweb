732b1c5d604ed86dca9b1f928d811b8208462f6a
xiu-du-du <cnimmor@gmail.com>
2022-03-18 00:41:47 +0800

    增加示例

---
+++ dist/1/js/main.js
@@ -0,0 +1,9 @@
+console.log(dom);
+console.log(dom.class);
+app.append(dom.create('<li><a href="#">首页</a></li>'))
+app.append(dom.create('<li><a href="#">概述</a></li>'))
+app.append(dom.create('<li><a href="#">资讯</a></li>'))
+app.append(dom.create('<li><a href="#">藏品</a></li>'))
+app.append(dom.create('<li><a href="#">展览</a></li>'))
+app.append(dom.create('<li><a href="#">研究</a></li>'))
+app.append(dom.create('<li><a href="#">社会服务</a></li>'))
